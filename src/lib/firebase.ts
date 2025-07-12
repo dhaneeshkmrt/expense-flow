e5fde1ab4d1b7795f5ca9172822d3490ead0d76f
Dhaneesh Kumar <dhaneeshkmrt@gmail.com>
2025-07-12 07:53:41 +0000

    i think still the api is failing

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyBnhiJ4H7QFRJve75x_zuQj-HFk34u1Pe0",
+  authDomain: "expenseflow-54862.firebaseapp.com",
+  projectId: "expenseflow-54862",
+  storageBucket: "expenseflow-54862.firebasestorage.app",
+  messagingSenderId: "457212274922",
+  appId: "1:457212274922:web:eec997b72d073022c0cf93",
+  measurementId: "G-LW7QDMHECM"
 };
 
 // Initialize Firebase
