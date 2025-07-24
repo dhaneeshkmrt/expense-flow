df41b6128cfcf7e8e6237b0497ef9baacefbc92f
Dhaneesh Kumar <dhaneeshkmrt@gmail.com>
2025-07-24 13:56:46 +0000

    key updated

--- workspace/src/lib/firebase.ts
+++ workspace/src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey:'AIzaSyCiSGpV5tBNgkQmg0bYXc8XiOslGk2eBEM',
+  authDomain: 'money-purse-7aee4.firebaseapp.com',
+  projectId: "money-purse-7aee4",
+  storageBucket:'money-purse-7aee4.firebasestorage.app',
+  messagingSenderId: "714772765127",
+  appId: "1:714772765127:web:234c8899a08159dab121e2",
+  measurementId: "G-EXZFQ0KECN"
 };
 
 // Initialize Firebase
