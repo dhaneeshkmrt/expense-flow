f906b398bbb55673d88bede6de2ff0d880aec041
Dhaneesh Kumar <dhaneeshkmrt@gmail.com>
2025-08-03 17:11:53 +0000

    netlify hosting build throw this error. how to fix this
     Scanning compl

--- workspace/src/lib/firebase.ts
+++ workspace/src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:'AIzaSyCiSGpV5tBNgkQmg0bYXc8XiOslGk2eBEM',
-  authDomain: 'money-purse-7aee4.firebaseapp.com',
-  projectId: "money-purse-7aee4",
-  storageBucket:'money-purse-7aee4.firebasestorage.app',
-  messagingSenderId: "714772765127",
-  appId: "1:714772765127:web:234c8899a08159dab121e2",
-  measurementId: "G-EXZFQ0KECN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
