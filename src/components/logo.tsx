214def9b0bbcc952d5efd075ffb88d376519ab6f
Dhaneesh Kumar <dhaneeshkmrt@gmail.com>
2025-08-31 06:30:36 +0000

    in the categories page add button to fill category budget from the previ

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import firebaseImageLoader from '../../loader';
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex justify-center items-center gap-2">
       <h1 className="text-xl font-bold tracking-tight text-primary">Money Purse</h1>
     </div>
   );
