99eae6fd827501b55c0a0b88d7028e6ed1e82db3
Dhaneesh Kumar <dhaneeshkmrt@gmail.com>
2025-11-03 13:23:10 +0000

    add new transaction page calendar date view has taking full width. make

--- src/components/transactions/add-transaction-sheet.tsx
+++ src/components/transactions/add-transaction-sheet.tsx
@@ -385,7 +385,7 @@ export default function AddTransactionSheet({
                             selected={field.value}
                             onSelect={field.onChange}
                             disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
-                            className="rounded-md border"
+                            className="rounded-md border inline-block"
                           />
                         )}
                         <FormMessage />
