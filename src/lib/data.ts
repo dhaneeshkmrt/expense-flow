4bb3b1fb254e34cfce037c376db5a78b2e0b1890
Dhaneesh Kumar <dhaneeshkmrt@gmail.com>
2025-11-06 15:33:24 +0000

    default categories updated

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -21,50 +21,17 @@ export const categories: Omit<Category, 'id' | 'tenantId'>[] = [
   {
     name: 'Monthly',
     icon: Calendar,
-    budget: 50000,
+    budget: 15000,
     subcategories: [
       { id: 'monthly_grocery', name: 'Grocery', microcategories: [] },
       { id: 'monthly_petrol', name: 'petrol', microcategories: [] },
       { id: 'monthly_veg', name: 'Veg', microcategories: [] },
       { id: 'monthly_non_veg', name: 'Non-veg', microcategories: [] },
       { id: 'monthly_gas', name: 'Gas', microcategories: [] },
-      { id: 'monthly_dk_appa', name: 'Dk Appa', microcategories: [] },
-      { id: 'monthly_nisha_amma', name: 'Nisha Amma', microcategories: [] },
       { id: 'monthly_milk', name: 'Milk', microcategories: [] },
       { id: 'monthly_egg', name: 'Egg', microcategories: [] },
-      { id: 'monthly_anna_exp', name: 'Anna Exp', microcategories: [] },
       { id: 'monthly_electrical', name: 'Electrical', microcategories: [] },
       { id: 'monthly_others', name: 'Others', microcategories: [] },
-    ],
-  },
-  {
-    name: 'Food & Snack',
-    icon: Utensils,
-    budget: 5000,
-    subcategories: [
-      { id: 'food_snack_food', name: 'Food', microcategories: [] },
-      { id: 'food_snack_snacks', name: 'Snacks', microcategories: [] },
-    ],
-  },
-  {
-    name: 'Fruits',
-    icon: Apple,
-    budget: 3000,
-    subcategories: [
-        { id: 'fruits_fruits', name: 'Fruits', microcategories: [] },
-        { id: 'fruits_nuts', name: 'Nuts', microcategories: [] },
-    ]
-  },
-  {
-    name: 'Gift',
-    icon: Gift,
-    budget: 5000,
-    subcategories: [
-        { id: 'gift_relatives', name: 'Relatives', microcategories: [] },
-        { id: 'gift_close_relatives', name: 'Close Relatives', microcategories: [] },
-        { id: 'gift_friends', name: 'Friends', microcategories: [] },
-        { id: 'gift_neighbour', name: 'Neighbour', microcategories: [] },
-        { id: 'gift_donation', name: 'Donation', microcategories: [] },
     ],
   },
   {
@@ -84,9 +51,30 @@ export const categories: Omit<Category, 'id' | 'tenantId'>[] = [
     ],
   },
   {
+    name: 'Food,Snacks & Outing',
+    icon: Utensils,
+    budget: 5000,
+    subcategories: [
+      { id: 'food_snack_food', name: 'Food', microcategories: [] },
+      { id: 'food_snack_snacks', name: 'Snacks', microcategories: [] },
+    ],
+  },
+  {
+    name: 'Gift',
+    icon: Gift,
+    budget: 5000,
+    subcategories: [
+        { id: 'gift_relatives', name: 'Relatives', microcategories: [] },
+        { id: 'gift_close_relatives', name: 'Close Relatives', microcategories: [] },
+        { id: 'gift_friends', name: 'Friends', microcategories: [] },
+        { id: 'gift_neighbour', name: 'Neighbour', microcategories: [] },
+        { id: 'gift_donation', name: 'Donation', microcategories: [] },
+    ],
+  },
+  {
     name: 'Tour',
     icon: Plane,
-    budget: 20000,
+    budget: 5000,
     subcategories: [
         { id: 'tour_travel', name: 'Travel', microcategories: [] },
         { id: 'tour_stay', name: 'Stay', microcategories: [] },
@@ -101,7 +89,7 @@ export const categories: Omit<Category, 'id' | 'tenantId'>[] = [
   {
     name: 'Emergency',
     icon: ShieldAlert,
-    budget: 10000,
+    budget: 5000,
     subcategories: [
         { id: 'emergency_home', name: 'Home', microcategories: [] },
         { id: 'emergency_medical', name: 'Medical', microcategories: [] },
@@ -112,84 +100,14 @@ export const categories: Omit<Category, 'id' | 'tenantId'>[] = [
     ],
   },
   {
-    name: 'Loan',
-    icon: RefreshCw,
-    budget: 0,
-    subcategories: [
-        { id: 'loan_category_transfer', name: 'Category Transfer', microcategories: [] },
-    ],
-  },
-  {
-    name: 'Home',
-    icon: Home,
-    budget: 15000,
-    subcategories: [
-        { id: 'home_electrical', name: 'Electrical', microcategories: [] },
-        { id: 'home_plumbing', name: 'Plumbing', microcategories: [] },
-        { id: 'home_construction', name: 'Construction', microcategories: [] },
-        { id: 'home_utensils', name: 'Utensils', microcategories: [] },
-        { id: 'home_farm_garden', name: 'Farm & Garden', microcategories: [] },
-        { id: 'home_decoration', name: 'Decoration', microcategories: [] },
-        { id: 'home_land_maintenance', name: 'Land Maintenance', microcategories: [] },
-    ],
-  },
-  {
-    name: 'DK professional',
+    name: 'Invesment',
     icon: Briefcase,
     budget: 5000,
     subcategories: [
-        { id: 'dk_professional_subscriptions', name: 'Subscriptions', microcategories: [] },
-        { id: 'dk_professional_meetups', name: 'Meetups', microcategories: [] },
-        { id: 'dk_professional_shopping', name: 'Shopping', microcategories: [] },
+        { id: 'invesment_fd', name: 'Fixed Deposit', microcategories: [] },
+        { id: 'invesment_mutual_fund', name: 'Mutual Fund', microcategories: [] },
     ],
-  },
-  {
-    name: 'Nisha',
-    icon: User,
-    budget: 10000,
-    subcategories: [
-        { id: 'nisha_invesment', name: 'Invesment', microcategories: [] },
-        { id: 'nisha_mobile', name: 'Mobile', microcategories: [] },
-        { id: 'nisha_personal_grooming', name: 'Personal Grooming', microcategories: [] },
-        { id: 'nisha_gift', name: 'Gift', microcategories: [] },
-        { id: 'nisha_office', name: 'Office', microcategories: [] },
-        { id: 'nisha_borrow', name: 'Borrow', microcategories: [] },
-        { id: 'nisha_cash_rtn', name: 'Cash Rtn', microcategories: [] },
-    ],
-  },
-  {
-    name: 'Dk',
-    icon: User,
-    budget: 10000,
-    subcategories: [
-        { id: 'dk_invesment', name: 'Invesment', microcategories: [] },
-        { id: 'dk_mobile_internet', name: 'Mobile&Internet', microcategories: [] },
-        { id: 'dk_personal_grooming', name: 'Personal Grooming', microcategories: [] },
-        { id: 'dk_gift', name: 'Gift', microcategories: [] },
-        { id: 'dk_borrow', name: 'Borrow', microcategories: [] },
-        { id: 'dk_subscriptions', name: 'Subscriptions', microcategories: [] },
-        { id: 'dk_others', name: 'Others', microcategories: [] },
-        { id: 'dk_office_expense', name: 'Office expense', microcategories: [] },
-        { id: 'dk_cash_rtn', name: 'Cash Rtn', microcategories: [] },
-    ],
-  },
-  {
-    name: 'WFO',
-    icon: Building,
-    budget: 25000,
-    subcategories: [
-        { id: 'wfo_travel', name: 'Travel', microcategories: [] },
-        { id: 'wfo_food', name: 'Food', microcategories: [] },
-        { id: 'wfo_home', name: 'Home', microcategories: [] },
-        { id: 'wfo_gift', name: 'Gift', microcategories: [] },
-        { id: 'wfo_local_travel', name: 'Local Travel', microcategories: [] },
-        { id: 'wfo_internet_subscriptions', name: 'Internet+ subscriptions', microcategories: [] },
-        { id: 'wfo_sports', name: 'Sports', microcategories: [] },
-        { id: 'wfo_monthly', name: 'Monthly', microcategories: [] },
-        { id: 'wfo_dmart', name: 'DMart', microcategories: [] },
-        { id: 'wfo_quick_commerce_app', name: 'Quick Commerce App', microcategories: [] },
-    ],
-  },
+  }
 ];
 
 
